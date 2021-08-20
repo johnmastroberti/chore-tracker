0d36568166ff8e8a491d27ed86af6d11b4312dd9
John Mastroberti <johnmastroberti123@gmail.com>
2021-08-20 15:35:40 -0400

    Cleaner static file serving

--- index.js
+++ index.js
@@ -17,13 +17,12 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get('/main.css', (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "main.css"));
-});
-
-app.get('/main.js', (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "main.js"));
-});
+const STATIC_FILES = ["main.css", "main.js", "apple-touch-icon.png", "favicon.ico"];
+for (f of STATIC_FILES) {
+  app.get('/' + f, (req, res) => {
+    res.sendFile(path.join(__dirname, "public", req.url));
+  });
+}
 
 const host = fs.readFileSync('/etc/hostname');
 let HOST = "localhost";
